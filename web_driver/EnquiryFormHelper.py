9275f97c4d717c41bad99b0c0245dbc3d1804236
ami05 <sarthak.gupta@soti.net>
2019-03-20 20:51:07 +0530

    updated url

--- web_driver/EnquiryFormHelper.py
+++ web_driver/EnquiryFormHelper.py
@@ -4,7 +4,7 @@ import time
 class EnquiryFormHelper():
 
     #__url = "http://blazedemo.com/"
-    __url = "https://52.25.64.193/front/default/newhome"
+    __url = "https://shoesonloose.com"
 
     def Open(self):
         self._web.open(self.__url)
